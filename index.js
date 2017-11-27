c0620fd05368796ccfb4f0f7262cfa4c7b9b5f11
pekko1215 <pekko1215@yahoo.co.jp>
2017-11-27 11:34:21 +0900

    tokens

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const Twitter = require('twitter');
 const Scanner = require('./Scanner.js')
-var client = new Twitter({
-	consumer_key:'oknbVRDJruiH9awtBHvMUo7IX',
-	consumer_secret:'KWNDC0FMS85O8qe7Cd9utO0eu4N9TLrxHyb1NGozUGcsrp7nkT',
-	access_token_key:'1965603450-0azP0BTp5Q6HNsmCkgdYjWdoCEXwwuCj7ZjsyD4',
-	access_token_secret:'UFFrVv89DCjDklTWpfWy1wVbNef3c5AGgUudPB0mXBYFm'
-})
+const Configs = require('./Configs.json');
+var client = new Twitter(Configs.tokens)
 
 client.stream('user',(stream)=>{
 	stream.on('data',(data)=>{
 		if(data.in_reply_to_user_id_str){return}
-		if(data.user.screen_name!='eakonnsamui'){return}
+		if(data.user.screen_name!=Configs.userName){return}
 		var flag = false;
 		Scanner.forEach((s)=>{
 			var reg = new RegExp(s,'g');
